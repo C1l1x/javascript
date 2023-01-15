b070c728717d31974b3ad71119c41af7c068ec3b
C1l1x <pavlov_s@abv.bg>
2023-01-15 11:41:34 +0200

    task1

--- 01.2_JS/zadacha1.js
+++ 01.2_JS/zadacha1.js
@@ -1,5 +1,5 @@
 /* 
-Napishete funkciq, koqto otpechatva chislata ot 1 do 10 na otdelni redove na konzolata
+Napishete funkciq, koqto otpechatva chislata ot 1 do 10 na otdelni redove na konzolata.
 */
 
 function nums1to10() {
